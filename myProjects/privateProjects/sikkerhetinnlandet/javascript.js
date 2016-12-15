ce442fe88a7eab02496607615789dcaaa7588824
Joachim <Joachim>
2016-12-15 14:41:47 +0100

    try agaiN

--- myProjects/privateProjects/sikkerhetinnlandet/javascript.js
+++ myProjects/privateProjects/sikkerhetinnlandet/javascript.js
@@ -1,51 +1,21 @@
 window.onload = function () {
     toggleMenuButton();
-    sendMail();
 }
 
 function toggleMenuButton() {
     "use strict";
-    let toggles = document.querySelectorAll(".menuButtonAnimation");
-    for (let i = toggles.length - 1; i >= 0; i--) {
-        let toggle = toggles[i];
-        toggleHandler(toggle);
-    };
+    let toggle = document.querySelector(".menuButtonAnimation"),
+        sidenav = document.getElementById('mySidenav'),
+        brand = document.getElementsByClassName('brand')[0],
+        noScroll = document.getElementsByClassName('noScroll')[0],
+        introduction = document.getElementById('introduction');
 
-    function toggleHandler(toggle) {
-        let sidenav = document.getElementById('mySidenav'),
-            pushed = document.getElementsByClassName('pushMe'),
-            brand = document.getElementsByClassName('brand')[0];
-        toggle.addEventListener("click", function (e) {
-            e.preventDefault();
-            if (this.classList.contains("is-active") === true) {
-                this.classList.remove("is-active");
-                sidenav.classList.remove('sidenavOpen');
-                for (let i = 0; i < pushed.length; i++) {
-                    pushed[i].classList.remove('pushed');
-                }
-                sidenav.classList.add('sidenavClosed');
-                brand.classList.remove('removeBrand')
-                console.log("Sidenav: closed");
-            } else {
-                this.classList.add("is-active");
-                brand.classList.add('removeBrand');
-                sidenav.classList.remove('sidenavClosed');
-                sidenav.classList.add('sidenavOpen');
-                for (let i = 0; i < pushed.length; i++) {
-                    pushed[i].classList.add('pushed');
-                }
-                console.log("Sidenav: open");
-            };
-        });
-    }
+    toggle.addEventListener("click", function (e) {
+        e.preventDefault();
+        this.classList.toggle("is-active");
+        sidenav.classList.toggle('sidenavOpen');
+        brand.classList.toggle('removeBrand');
+        noScroll.classList.toggle('overflow');
+        scroll(0, 0);
+    });
 };
-
-function sendMail(){
-    document.getElementById("mailForm").onclick = function() {
-    var wnd = window.open("https://formspree.io/thanks?next=https%3A%2F%2Fbattgi.github.io%2FmyProjects%2FprivateProjects%2Fsikkerhetinnlandet%2Findex.html");
-    setTimeout(function() {
-      wnd.close();
-    }, 2000);
-    return false;
-  };
-}
